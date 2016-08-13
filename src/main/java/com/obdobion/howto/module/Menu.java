6464da2879585828ccd590fbaf5e7171c3d77e90
Chris DeGreef <fedupforone@gmail.com>
2016-08-13 12:08:18 -0500

    initial load

--- src/main/java/com/obdobion/howto/module/Menu.java
+++ src/main/java/com/obdobion/howto/module/Menu.java
@@ -1,18 +1,12 @@
 package com.obdobion.howto.module;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.text.ParseException;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.regex.Pattern;
 
-import com.obdobion.argument.CmdLine;
-import com.obdobion.argument.ICmdLine;
 import com.obdobion.argument.annotation.Arg;
 import com.obdobion.howto.Context;
 import com.obdobion.howto.IPlugin;
@@ -23,15 +17,14 @@ import com.obdobion.howto.IPlugin;
  */
 public class Menu implements IPlugin
 {
-    @Arg(shortName = 'm', help = "Only commands matching all patterns will be displayed.", caseSensitive = true)
+    @Arg(shortName = 'm', help = "Only commands matching all patterns will be displayed.")
     private Pattern[] matches;
 
     @Arg(allowCamelCaps = true, shortName = 's')
     private boolean   sortDescending;
 
     public Menu()
-    {
-    }
+    {}
 
     private boolean allMatchersMatch(final String output)
     {
@@ -85,25 +78,5 @@ public class Menu implements IPlugin
     public String getOverview()
     {
         return "Shows a list of commands";
-    }
-
-    private void verbose(final IPlugin plugin, final PrintWriter pw, final String args)
-    {
-        final ICmdLine cmdline = new CmdLine(plugin.getName(), plugin.getOverview());
-        try
-        {
-            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            final PrintStream ps = new PrintStream(baos);
-            final PrintStream oldOut = System.out;
-            System.setOut(ps);
-            CmdLine.load(cmdline, plugin, args);
-            System.setOut(oldOut);
-
-            pw.printf("%1$s\n\n", baos.toString());
-
-        } catch (IOException | ParseException e)
-        {
-            pw.printf("%1$-10s%2$s\n    %3$s\n\n", plugin.getName(), plugin.getOverview(), e.getMessage());
-        }
     }
 }
